f7a23056accbc0eb5e4b50b446d481d79e7cb758
avisrabon22@gmail.com <avisrabon22@gmail.com>
2022-09-24 04:08:06 +0530

    first commit 24-09-22

---
+++ src/main/java/com/pcs/assetmanager/AssetsmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.pcs.assetmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetsmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetsmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pcs/assetmanager/AssetsmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcs.assetmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetsmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
