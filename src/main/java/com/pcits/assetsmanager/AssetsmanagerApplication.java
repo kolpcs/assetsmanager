afbc863eca08ccc4527e50e7e8033115c6f5b651
avisrabon22@gmail.com <avisrabon22@gmail.com>
2022-10-29 11:50:34 +0530

    online new update

--- src/main/java/com/pcits/assetsmanager/AssetsmanagerApplication.java
+++ src/main/java/com/pcits/assetsmanager/AssetsmanagerApplication.java
@@ -1,4 +1,4 @@
-package com.pcs.assetmanager;
+package com.pcits.assetsmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,14 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AssetsmanagerApplication {
 
 	public static void main(String[] args) {
-		
-		int a=9;
-		int b=0;
-		int ab= a+b;
-		
-		int addition=ab+34;
-		
-		
 		SpringApplication.run(AssetsmanagerApplication.class, args);
 	}
 
--- src/test/java/com/pcits/assetsmanager/AssetsmanagerApplicationTests.java
+++ src/test/java/com/pcits/assetsmanager/AssetsmanagerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.pcs.assetmanager;
+package com.pcits.assetsmanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
