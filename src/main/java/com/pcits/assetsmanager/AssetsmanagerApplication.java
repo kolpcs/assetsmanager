f405de308f6a8ebf4f518a2aca584233bb5a6f0c
avisrabon22@gmail.com <avisrabon22@gmail.com>
2022-11-10 02:31:49 +0530

    update

--- src/main/java/com/pcits/assetsmanager/AssetsmanagerApplication.java
+++ src/main/java/com/pcits/assetsmanager/AssetsmanagerApplication.java
@@ -6,14 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AssetsmanagerApplication {
 
-<<<<<<< HEAD
-    public static void main(String[] args) {
-        SpringApplication.run(AssetsmanagerApplication.class, args);
-    }
-=======
 	public static void main(String[] args) {
 		SpringApplication.run(AssetsmanagerApplication.class, args);
 	}
->>>>>>> work/testing
 
 }
