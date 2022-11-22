b4323166a452230a85627acedb359282039e865b
avisrabon22@gmail.com <avisrabon22@gmail.com>
2022-11-23 03:19:01 +0530

    front added

--- src/main/java/com/pcits/assetsmanager/Controller/mainController.java
+++ src/main/java/com/pcits/assetsmanager/Controller/mainController.java
@@ -4,26 +4,16 @@ import com.pcits.assetsmanager.Model.userDetails;
 import com.pcits.assetsmanager.repo.mongoDBRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-
-@RequestMapping(value = "/usersDetails")
+@Controller
 public class mainController {
-    @Autowired
-    private mongoDBRepo userRepo;
 
-    @PostMapping("/")
-    public ResponseEntity<?> addUser(userDetails users) {
-        userDetails saved = this.userRepo.save(users);
-        return ResponseEntity.ok(saved);
-    }
-
-    @GetMapping("/")
-    public ResponseEntity<?> getUser() {
-        return ResponseEntity.ok(this.userRepo.findAll());
-    }
 }
+
+
